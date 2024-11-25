f72f83f4bcab64b1bc8aab88357c915becbe8033
Mohammad Amin Feli Akbari <mfly22317@gmail.com>
2024-11-25 22:14:28 +0330

    Add print hello in line 37

--- Main.java
+++ Main.java
@@ -34,6 +34,8 @@ public class Main {
                 return null;
             }
 
+            System.out.println("hello");
+
             Node newNode = new Node(Integer.parseInt(str[root]));
 
             newNode.setLeftNode(inOrder_tree(str, root * 2 + 1));
